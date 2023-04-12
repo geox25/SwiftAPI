4992a0327e4109489ec30a944825907837e297b7
geox25 <98139887+geox25@users.noreply.github.com>
2023-04-12 18:24:37 -0500

    Create SwiftAPI.java

---
+++ src/main/java/me/geox25/swifteco/SwiftAPI.java
@@ -0,0 +1,38 @@
+package me.geox25.swifteco;
+
+import java.util.List;
+
+public interface SwiftAPI {
+
+    /*
+    Sets a property of a bank account
+     */
+    public void setAccountProperty(String path, String prop);
+    public void setAccountProperty(String path, int prop);
+    public void setAccountProperty(String path, double prop);
+
+    /*
+    Gets a property of a bank account
+     */
+    public String getAccountProperty(String path);
+
+    /*
+    Checks if a bank account exists
+     */
+    public boolean accountExists(String account);
+
+    /*
+    Gets the managers of a bank account
+     */
+    public List<String> getAccountManagers(String account);
+
+    /*
+    Checks if player is a manager of the specified account
+     */
+    public boolean isManager(String account, String playerName);
+
+    /*
+    Gets the balance of a bank account
+     */
+    public double getBalance(String account);
+}
