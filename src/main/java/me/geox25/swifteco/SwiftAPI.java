b4953ea74785a9dd8c3027de976a7686f6ca5abd
Geo <98139887+geox25@users.noreply.github.com>
2023-07-13 14:39:19 -0500

    Update SwiftAPI.java

--- src/main/java/me/geox25/swifteco/SwiftAPI.java
+++ src/main/java/me/geox25/swifteco/SwiftAPI.java
@@ -4,44 +4,14 @@ import java.util.List;
 
 public interface SwiftAPI {
 
-    /*
-    Sets a property of a bank account
-     */
-    public void setAccountProperty(String path, String prop);
-    
-    public void setAccountProperty(String path, int prop);
-    
-    public void setAccountProperty(String path, double prop);
-
-    /*
-    Gets a property of a bank account
-     */
+    // Property Management Methods
+    public void setAccountProperty(String path, String property);
     public String getAccountProperty(String path);
-    
-    public double getAccountDoubleProperty(String path);
 
-    /*
-    Checks if a bank account exists
-     */
-    public boolean accountExists(String account);
-
-    /*
-    Gets the managers of a bank account
-     */
-    public List<String> getAccountManagers(String account);
-
-    /*
-    Checks if player is a manager of the specified account
-     */
-    public boolean isManager(String account, String playerName);
-
-    /*
-    Gets the balance of a bank account
-     */
-    public double getBalance(String account);
-
-    /*
-    Sets the balance of a bank account
-     */
-    public void setBalance(String account, double balance);
+    // Account Management Methods
+    public boolean accountExists(String path);
+    public List<String> getAccountManagers(String path);
+    public boolean isManager(String path, String uuid);
+    public void setBalance(String path, double balance);
+    public double getBalance(String path);
 }
